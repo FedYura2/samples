7171a0abbe73b62a622353a7e89cc6145b5d507b
Yura Fedorov <dmdofer@gmail.com>
2018-05-28 09:34:50 +0300

    first commit

---
+++ OrdersWatcher.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using xxx.xxx.Domain.Abstract;
+using xxx.xxx.Domain.Models;
+using xxx.xxx.Services.Configurations.Abstract;
+using xxx.xxx.Services.Configurations.Helper;
+using xxx.xxx.Services.Logging;
+
+namespace xxx.xxx.Domain.Logics
+{
+    public interface IOrdersWatcher
+    {
+        void StartOrderTracking(OrderIdentifier orderIdentifier, string exchange);
+        void StopOrderTracking(OrderIdentifier orderIdentifier, string exchange);
+        event EventHandler<OrderStatusUpdateArgs> OnOrderStatusUpdated;
+    }
+
+    public class OrdersWatcher : IOrdersWatcher
+    {
+        private readonly IConfigurationsAdapter _configurationsAdapter;
+        private readonly IExchangesManager _exchangesManager;
+        private readonly ILogger _logger;
+
+        private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, OrderIdentifier>> _ordersToCheck;
+
+        public event EventHandler<OrderStatusUpdateArgs> OnOrderStatusUpdated;
+
+        #region Configurations
+
+        private int _updateTimeForUpdatesOrdersInMs = 2000;
+
+        #endregion
+
+        public OrdersWatcher(IConfigurationsAdapter configurationsAdapter, IExchangesManager exchangesManager, ILogger logger)
+        {
+            _configurationsAdapter = configurationsAdapter;
+            _exchangesManager = exchangesManager;
+            _logger = logger ?? new DefaultLogger();
+            _ordersToCheck = new ConcurrentDictionary<string, ConcurrentDictionary<string, OrderIdentifier>>();
+            InitConfig();
+            StartTracking();
+        }
+
+        public void StopOrderTracking(OrderIdentifier orderIdentifier, string exchange)
+        {
+            try
+            {
+                if (_ordersToCheck.TryGetValue(exchange, out ConcurrentDictionary<string, OrderIdentifier> collection) &&
+                    collection.ContainsKey(orderIdentifier.Value) && collection.TryRemove(orderIdentifier.Value, out OrderIdentifier order))
+                {
+                    _logger?.Log(SeverityLevel.Debug, $"Order: {order}; Exchange: {exchange}. Removed from tracking service!");
+                }
+                else
+                {
+                    _logger?.Log(SeverityLevel.Debug, $"Order: {orderIdentifier}; Exchange: {exchange}. Not found! Can't removed from tracking service!");
+                }
+            }
+            catch (Exception e)
+            {
+                _logger?.Log(SeverityLevel.Error, $"Failed to remove order: {orderIdentifier}; Exchange: {exchange} from tracking service!");
+                _logger?.Log(SeverityLevel.Error, e);
+            }
+        }
+
+        public void StartOrderTracking(OrderIdentifier orderIdentifier, string exchange)
+        {
+            try
+            {
+                var newCollection = new ConcurrentDictionary<string, OrderIdentifier>();
+                newCollection.AddOrUpdate(orderIdentifier.Value, orderIdentifier, (s, identifier) => orderIdentifier);
+                _ordersToCheck.AddOrUpdate(exchange, newCollection,
+                    (s, list) =>
+                    {
+                        list.AddOrUpdate(orderIdentifier.Value, orderIdentifier,
+                            (s1, identifier) => orderIdentifier);
+                        return list;
+                    });
+                _logger?.Log(SeverityLevel.Debug, $"Add order: {orderIdentifier}; Exchange: {exchange}. To tracking service.");
+            }
+            catch (Exception e)
+            {
+                _logger?.Log(SeverityLevel.Error, $"Failed to add order: {orderIdentifier}; Exchange: {exchange}. To tracking service!");
+                _logger?.Log(SeverityLevel.Error, e);
+            }
+        }
+
+
+        private void InitConfig()
+        {
+            try
+            {
+                _configurationsAdapter.SubscribeToConfigurationChange(DomainLogicSettings.ORDERS_WATCHER_UPDATE_TIME_FOR_UPDATES_ORDERS,
+                    configuration =>
+                    {
+                        if (configuration.OldValue != configuration.NewValue && int.TryParse(configuration.NewValue, out int updateTime))
+                        {
+                            _updateTimeForUpdatesOrdersInMs = updateTime;
+                        }
+                    });
+                var frequentUpdateTime = _configurationsAdapter.GetConfiguration(DomainLogicSettings
+                    .ORDERS_WATCHER_UPDATE_TIME_FOR_UPDATES_ORDERS);
+                if (!string.IsNullOrEmpty(frequentUpdateTime) && int.TryParse(frequentUpdateTime, out int value))
+                {
+                    _updateTimeForUpdatesOrdersInMs = value;
+                }
+            }
+            catch (Exception e)
+            {
+                _logger?.Log(SeverityLevel.Error, e);
+            }
+        }
+
+        private void StartTracking()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                var allExchanges = _ordersToCheck.Keys.ToList();
+                while (true)
+                {
+                    var allExchangesNew = _ordersToCheck.Keys.ToList();
+                    if (!allExchanges.SequenceEqual(allExchangesNew))
+                    {
+                        var newExchanges = allExchangesNew.Where(x => allExchanges.All(e => e != x)).ToList();
+                        foreach (var newExchange in newExchanges)
+                        {
+                            StartTrackingFrequentOrdersForExchange(newExchange);
+                        }
+                    }
+                    allExchanges = allExchangesNew;
+                    Thread.Sleep(1000);
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+
+        private void StartTrackingFrequentOrdersForExchange(string newExchange)
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                var lastCkeck = DateTime.UtcNow;
+                while (true)
+                {
+                    IExchange exchange;
+                    try
+                    {
+                        exchange = _exchangesManager.GetExchangeByName(newExchange);
+
+                        if (_ordersToCheck.TryGetValue(newExchange, out ConcurrentDictionary<string, OrderIdentifier> collection))
+                        {
+                            await Task.WhenAll(collection.Values.ToList()
+                                .Select(async x =>
+                                {
+                                    try
+                                    {
+                                        var state = await exchange.GetOrderStatusAsync(x);
+                                        Volatile.Read(ref OnOrderStatusUpdated)?.Invoke(this, new OrderStatusUpdateArgs()
+                                        {
+                                            OrderIdentifier = x,
+                                            OrderStatus = state,
+                                            Exchange = newExchange
+                                        });
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        _logger?.Log(SeverityLevel.Error, $"Failed to update state for order: {x}; Exchange: {newExchange}!");
+                                        _logger?.Log(SeverityLevel.Error, e);
+                                    }
+                                }));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        _logger?.Log(SeverityLevel.Info, $"Can't get exchange {newExchange}!");
+                        _logger?.Log(SeverityLevel.Info, e);
+                    }
+                    var diff = (DateTime.UtcNow - lastCkeck).TotalMilliseconds;
+                    if (diff < _updateTimeForUpdatesOrdersInMs)
+                    {
+                        Thread.Sleep(TimeSpan.FromMilliseconds(_updateTimeForUpdatesOrdersInMs - diff));
+                    }
+                    lastCkeck = DateTime.UtcNow;
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+    }
+}
---
+++ OrdersWatcherManager.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using xxx.xxx.Domain.Abstract;
+using xxx.xxx.Domain.Models;
+using xxx.xxx.Domain.Models.DB;
+using xxx.xxx.Services.Logging;
+
+namespace xxx.xxx.Domain.Logics
+{
+    public interface IOrdersWatcherManager
+    {
+        void StartOrderTracking(OpenOrderModel order);
+        void StopOrderTracking(OpenOrderModel orderIdentifier);
+
+        IDictionary<string, List<OpenOrderModel>> GetAllOrders();
+        IList<OpenOrderModel> GetAllOrdersForExchange(string exchange);
+
+        event EventHandler<OpenOrderStatusUpdateArgs> OnOrderStatusUpdated;
+    }
+
+    public class OrdersWatcherManager : IOrdersWatcherManager
+    {
+        private readonly IOrdersWatcher _ordersWatcher;
+        private readonly IInstrumentsNameMapper _instrumentsNameMapper;
+        private readonly Func<DbContext> _contextCreator;
+        private readonly ILogger _logger;
+        private readonly IOrderCancelationWatcher _orderCancelationWatcher;
+        private readonly IDictionaryModelsMapper _dictionaryModelsMapper;
+
+        private readonly ConcurrentDictionary<string, OpenOrderModel> _allOrders;
+
+        public OrdersWatcherManager(IOrdersWatcher ordersWatcher, IInstrumentsNameMapper instrumentsNameMapper,
+            Func<DbContext> contextCreator, IOrderCancelationWatcher orderCancelationWatcher, ILogger logger,
+            IDictionaryModelsMapper dictionaryModelsMapper)
+        {
+            _ordersWatcher = ordersWatcher;
+            _instrumentsNameMapper = instrumentsNameMapper;
+            _contextCreator = contextCreator;
+            _orderCancelationWatcher = orderCancelationWatcher;
+            _dictionaryModelsMapper = dictionaryModelsMapper;
+            _logger = logger ?? new DefaultLogger();
+
+            _allOrders = new ConcurrentDictionary<string, OpenOrderModel>();
+
+            _ordersWatcher.OnOrderStatusUpdated += (sender, args) =>
+            {
+                try
+                {
+                    var openOrderStatusUpdateArgs = new OpenOrderStatusUpdateArgs
+                    {
+                        OrderIdentifier = new OrderIdentifier(args.OrderIdentifier.Value, _instrumentsNameMapper.GetGeneralInstrument(args.OrderIdentifier.Instrument.Name, args.Exchange)),
+                        OrderStatus = args.OrderStatus,
+                        OpenOrderModel = _allOrders[args.OrderIdentifier.Value]
+                    };
+                    Volatile.Read(ref OnOrderStatusUpdated)?.Invoke(this, openOrderStatusUpdateArgs);
+                }
+                catch (Exception e)
+                {
+                    _logger?.Log(SeverityLevel.Error, $"Failed to start send update event for Order {args.OrderIdentifier}");
+                    _logger?.Log(SeverityLevel.Error, e);
+                }
+            };
+            Task.Run(async () => await StartTrackingAllOpenOrders()).Wait();
+        }
+
+        public event EventHandler<OpenOrderStatusUpdateArgs> OnOrderStatusUpdated;
+
+        public void StartOrderTracking(OpenOrderModel order)
+        {
+            try
+            {
+                var exchange = _dictionaryModelsMapper.GetName<ExchangeModel>(order.ExchangeId);
+                var targetInstrument = _instrumentsNameMapper.GetExchangeInstrument(
+                    _dictionaryModelsMapper.GetName<InstrumentModel>(order.InstrumentId), exchange);
+
+                var orderIdentifier = new OrderIdentifier(order.OrderId, targetInstrument);
+                _ordersWatcher.StartOrderTracking(orderIdentifier, exchange);
+                _allOrders.AddOrUpdate(order.OrderId, order, (s, model) => order);
+                _orderCancelationWatcher.AddToWatcher(order);
+            }
+            catch (Exception e)
+            {
+                _logger?.Log(SeverityLevel.Error, $"Failed to start tracking for new order! Exchange: {order.ExchangeId}, OrderId = {order.OrderId}");
+                _logger?.Log(SeverityLevel.Error, e);
+                throw;
+            }
+        }
+
+        public void StopOrderTracking(OpenOrderModel order)
+        {
+            try
+            {
+                var exchange = _dictionaryModelsMapper.GetName<ExchangeModel>(order.ExchangeId);
+                var targetInstrument = _instrumentsNameMapper.GetExchangeInstrument(
+                    _dictionaryModelsMapper.GetName<InstrumentModel>(order.InstrumentId), exchange);
+                var orderIdentifier = new OrderIdentifier(order.OrderId, targetInstrument);
+
+                _allOrders.TryRemove(order.OrderId, out OpenOrderModel removedOrder);
+                _ordersWatcher.StopOrderTracking(orderIdentifier, exchange);
+                _orderCancelationWatcher.RemoveFromWatcher(order);
+            }
+            catch (Exception e)
+            {
+                _logger?.Log(SeverityLevel.Error, $"Failed to start tracking for new order! Exchange: {order.ExchangeId}, OrderId = {order.OrderId}");
+                _logger?.Log(SeverityLevel.Error, e);
+                throw;
+            }
+        }
+
+        public IDictionary<string, List<OpenOrderModel>> GetAllOrders()
+        {
+            return _allOrders.Values.GroupBy(x => x.ExchangeId).ToDictionary(x => _dictionaryModelsMapper.GetName<ExchangeModel>(x.Key), y => y.ToList());
+        }
+
+        public IList<OpenOrderModel> GetAllOrdersForExchange(string exchange)
+        {
+            var exchangeId = _dictionaryModelsMapper.GetId<ExchangeModel>(exchange);
+            var orders = _allOrders.Values.Where(x => x.ExchangeId == exchangeId).ToList();
+            return orders;
+        }
+
+        private async Task StartTrackingAllOpenOrders()
+        {
+            try
+            {
+                using (var db = _contextCreator())
+                {
+                    var allOpenedOrders = await db.Set<OpenOrderModel>()
+                        .Where(x => x.OrderStatus == OrderStatusType.Open)
+                        .Include(x => x.OriginalOrder)
+                        .Include(x => x.ResponceOrder)
+                        .ToListAsync();
+                    Parallel.ForEach(allOpenedOrders, openedOrder =>
+                    {
+                        try
+                        {
+                            var exchange = _dictionaryModelsMapper.GetName<ExchangeModel>(openedOrder.ExchangeId);
+                            var targetInstrument = _instrumentsNameMapper.GetExchangeInstrument(
+                                _dictionaryModelsMapper.GetName<InstrumentModel>(openedOrder.InstrumentId), exchange);
+                            _ordersWatcher.StartOrderTracking(new OrderIdentifier(openedOrder.OrderId, targetInstrument), exchange);
+                            _allOrders.AddOrUpdate(openedOrder.OrderId, openedOrder, (s, model) => openedOrder);
+                            _orderCancelationWatcher.AddToWatcher(openedOrder);
+                        }
+                        catch (Exception e)
+                        {
+                            _logger?.Log(SeverityLevel.Error, $"Failed to start tracking for order Id = {openedOrder.Id}!");
+                            _logger?.Log(SeverityLevel.Error, e);
+                        }
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                _logger?.Log(SeverityLevel.Error, "Failed to start tracking for orders!");
+                _logger?.Log(SeverityLevel.Error, e);
+                throw;
+            }
+        }
+    }
+}
